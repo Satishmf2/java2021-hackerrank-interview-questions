b11a052c1b78fbdefd9c5ac023028f1c71c064a9
satishmf2 <satishmf2@gmail.com>
2021-05-07 08:02:59 -0400

    Given an array A[] and a number x, check for pair in A[] with sum as x

--- src/main/java/SumAsX.java
+++ src/main/java/SumAsX.java
@@ -4,17 +4,20 @@ import java.util.HashSet;
 
 public class SumAsX {
     public static void main(String[] args) {
-        int A[] = {0, 1, 2, 3, 4, 5, 6};
+        int A[] = {1, 5, 7, -1, 5};
         int sum = 6;
+        int count=0;
         HashSet<Integer> hashSet = new HashSet<Integer>();
         for (int i = 0; i < A.length; i++) {
 
             int temp = sum - A[i];
             if (hashSet.contains(temp)) {
                 System.out.println(" " + A[i] + " &" + temp + " ");
+                count++;
             } else {
                 hashSet.add(A[i]);
             }
         }
+        System.out.println(count);
     }
 }
